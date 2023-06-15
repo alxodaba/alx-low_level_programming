274ff156b2f19b414fa21c61dee718c8087aea1a
alxodaba <kowodaba@live.com>
2023-06-15 12:04:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using printf function
- * Return: 0 (Success)
+ * *Description:  main - Prints out a string to stdout.
+ * *Return: 0 if success.
  */
 int main(void)
 {
