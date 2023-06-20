46a9bfef31513a1a816f616f4a278f9d9435156e
alxodaba <kowodaba@live.com>
2023-06-20 15:40:01 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,10 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-	for (i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
+
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
